18ed3c774309db105a71947ed9c64392fb5ab0c8
tmgyannis <bonfantiyannis@gmail.com>
2025-01-23 14:38:00 +0100

    premierefonction en C

---
+++ src/ft-isalpha.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// Déclaration de la fonction ft_isalpha
+int ft_isalpha(int c);
+
+int main(void)
+{
+    printf("Test avec 'A': %d\n", ft_isalpha('A')); // Doit retourner 1
+    printf("Test avec 'z': %d\n", ft_isalpha('z')); // Doit retourner 1
+    printf("Test avec '1': %d\n", ft_isalpha('1')); // Doit retourner 0
+    printf("Test avec '@': %d\n", ft_isalpha('@')); // Doit retourner 0
+    return (0);
+}
+
+// Définition de la fonction ft_isalpha
+int ft_isalpha(int c)
+{
+    if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+        return (1);
+    return (0);
+}
