aa968719f110fc51938f6b41dc65c0f97999ff73
tmgyannis <bonfantiyannis@gmail.com>
2025-01-28 14:53:26 +0100

    premierefonction en c

--- src/ft-bzero.c
+++ src/ft-bzero.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 
-void	ft_bzero(void *s, size_t n)
+void*	ft_bzero(void *b, size_t len)
 {
-	memset(s, 0, n);
+    unsigned char *ptr = (unsigned char *)b;
+
+    while (len > 0)
+    {
+        *ptr = (unsigned char)0; // Remplit l'octet courant avec la valeur
+        ptr++;
+        len--;
+    }
+
+    return (b); // Retourne le pointeur initial
 }
 
 int main(void)
@@ -14,7 +23,7 @@ int main(void)
     printf("Avant ft_bzero : %s\n", str);
 
     // Utilisation de ft_bzero pour mettre les 5 premiers caractères à 0
-    bzero(str, 5);
+    ft_bzero(str, 5);
 
     // Après bzero
     printf("Après ft_bzero : %s\n", str); // Les premiers caractères seront invisibles
@@ -25,4 +34,3 @@ int main(void)
 
     return 0;
 }
-
