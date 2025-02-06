a58fff912d42afae3057cdf8c849ba4980d7fefe
tmgyannis <bonfantiyannis@gmail.com>
2025-02-06 16:12:11 +0100

    premierefonction en c

--- src/ft_atoi.c
+++ src/ft_atoi.c
@@ -10,9 +10,9 @@ int main() {
 
     printf("atoi(\"42\") = %d\n", atoi(str1));   // 42
     printf("atoi(\"-123\") = %d\n", atoi(str2)); // -123
-    printf("atoi(\"  567\") = %d\n", atoi(str3)); // 567 (ignore les espaces)
-    printf("atoi(\"98abc\") = %d\n", atoi(str4)); // 98 (s'arrête au 'a')
-    printf("atoi(\"abc123\") = %d\n", atoi(str5)); // 0 (aucun chiffre au début)
+    printf("atoi(\"  567\") = %d\n", atoi(str3)); // 567 ignore les espaces
+    printf("atoi(\"98abc\") = %d\n", atoi(str4)); // 98 s'arrête au 'a'
+    printf("atoi(\"abc123\") = %d\n", atoi(str5)); // 0 aucun chiffre au début
 
     return 0;
 }
@@ -36,3 +36,4 @@ int ft_atoi(char *str)
     return (0);
 
 }
+
